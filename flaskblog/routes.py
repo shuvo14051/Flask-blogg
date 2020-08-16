2bea0c5798ec131e6d3f6fdab2334ac283e89a35
shuvo14051 <shuvo1137017@gmail.com>
2020-08-16 16:27:33 +0600

    f

---
+++ flaskblog/routes.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, url_for, flash, redirect, request
+from flaskblog.models import User, Post
+from flaskblog import app, db, bcrypt
+from flaskblog.forms import RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created. You are ready to login.', 'info')
+        return redirect(url_for('login'))
+    return render_template('register.html',  form=form)
+
+
+@app.route('/login',  methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash(f'Log in unsuccessfull. Enter a valid email and password', 'danger')
+
+    return render_template('login.html', form=form)   
+
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+
+@app.route('/account')
+@login_required
+def account():
+    image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
+    return render_template('account.html', image_file = image_file)   
